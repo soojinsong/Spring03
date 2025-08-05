9c6f35e626d7c2600701f6cefd9466f9a7157fb6
soojinsong <soojin.song00@gmail.com>
2025-08-05 15:41:26 +0900

    03:41 push

---
+++ src/main/java/pack/Spring03Application.java
@@ -0,0 +1,21 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Spring03Application {
+	
+	@GetMapping("/")
+	public String ttt() {
+		return "333";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring03Application.class, args);
+	}
+
+}
